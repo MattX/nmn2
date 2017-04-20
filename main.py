d9639b39f9e5de3d1e089350068132ced6435490
Matthieu Felix <matthieufelix@gmail.com>
2017-04-20 21:20:28 +0000

    remove vis from train

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
 
         logging.info(" Training set...")
         train_loss, train_acc, _ = \
-                do_iter(task.train, model, config, train=True, vis=True)
+                do_iter(task.train, model, config, train=True)
         val_loss, val_acc, val_predictions = \
                 do_iter(task.val, model, config, vis=True)
         test_loss, test_acc, test_predictions = \
